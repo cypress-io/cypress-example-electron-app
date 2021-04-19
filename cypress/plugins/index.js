fe0f2e03d5f5b62cef0fcb24af3d7e24f97685a1
Zach Bloomquist <git@chary.us>
2021-04-19 13:30:19 -0400

    chore: add prettier

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,24 +1,11 @@
 /// <reference types="cypress" />
 
-const fs = require("fs").promises;
-const assert = require("assert");
-const path = require("path");
-const { version, productName } = require("../../package.json");
+const fs = require('fs').promises
+const assert = require('assert')
+const path = require('path')
+const { version, productName } = require('../../package.json')
 
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-const debug = (...args) => console.log("Cypress pluginsfile:", ...args);
+const debug = (...args) => console.log('Cypress pluginsfile:', ...args)
 
 /**
  * Create a custom browser object pointing to our packaged Electron app.
@@ -26,69 +13,67 @@ const debug = (...args) => console.log("Cypress pluginsfile:", ...args);
 const getElectronBrowser = async () => {
   const browserPath = path.resolve(
     __dirname,
-    "../../out",
-    [productName, process.platform, process.arch].join("-"),
+    '../../out',
+    [productName, process.platform, process.arch].join('-'),
     productName
-  );
+  )
 
   try {
-    await fs.stat(browserPath);
+    await fs.stat(browserPath)
   } catch (err) {
     debug(
-      "fs.stat failed. Expected to find a built Electron app at",
+      'fs.stat failed. Expected to find a built Electron app at',
       browserPath,
-      "but an error occurred."
-    );
-    debug(
-      "Has the Electron app been built yet? Run `yarn package` to rebuild."
-    );
-    throw err;
+      'but an error occurred.'
+    )
+    debug('Has the Electron app been built yet? Run `yarn package` to rebuild.')
+    throw err
   }
 
   return {
-    // with name and family === 'chromium', Cypress will treat this custom browser like any other CDP browser
-    name: "chromium",
-    family: "chromium",
-    channel: "dev",
+    // With name and family === 'chromium', Cypress will treat this custom browser like any other CDP browser
+    name: 'chromium',
+    family: 'chromium',
+    channel: 'dev',
     displayName: `${productName} (Electron)`,
     version,
     path: browserPath,
-    majorVersion: version.split(".")[0],
-  };
-};
+    majorVersion: version.split('.')[0],
+  }
+}
 
 module.exports = async (on, config) => {
   // To keep it simple, this example focuses on only testing inside of Electron.
   // To that end, the default browsers detected by Cypress will be ignored.
   debug(
     config.browsers.length,
-    "browsers were detected by Cypress, ignoring..."
-  );
+    'browsers were detected by Cypress, ignoring...'
+  )
 
-  const browser = await getElectronBrowser();
+  const browser = await getElectronBrowser()
 
-  on("before:browser:launch", (launchingBrowser, launchOptions) => {
+  on('before:browser:launch', (launchingBrowser, launchOptions) => {
     assert.strictEqual(
       launchingBrowser.path,
       browser.path,
-      "Only the Electron app should be launched."
-    );
+      'Only the Electron app should be launched.'
+    )
 
-    const { args } = launchOptions;
+    const { args } = launchOptions
 
-    if (!args.find((arg) => arg.startsWith("--proxy-bypass-list"))) {
-      // Cypress automatically adds this argument when launching Chrome version 72+. 
+    if (!args.find((arg) => arg.startsWith('--proxy-bypass-list'))) {
+      // Cypress automatically adds this argument when launching Chrome version 72+.
       // Since we're passing our package version to Cypress, not the actual Chromium version, we need to add this ourselves.
-      args.push("--proxy-bypass-list=<-loopback>");
+      args.push('--proxy-bypass-list=<-loopback>')
     }
 
-    debug("Launching packaged Electron app with options:", launchOptions);
-    return launchOptions;
-  });
+    debug('Launching packaged Electron app with options:', launchOptions)
+    return launchOptions
+  })
 
-  debug("Returning custom Electron browser to Cypress:", browser);
+  debug('Returning custom Electron browser to Cypress:', browser)
 
   return {
     browsers: [browser],
-  };
-};
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
-const inCypress = process.env.IN_CYPRESS;
+const inCypress = process.env.IN_CYPRESS
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require("electron-squirrel-startup")) {
+if (require('electron-squirrel-startup')) {
   // eslint-disable-line global-require
-  app.quit();
+  app.quit()
 }
 
 const createWindow = () => {
@@ -14,35 +14,35 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-  });
+  })
 
   // at the time of this writing, Cypress expects the CDP browser to launch with an `about:blank` target
   inCypress
-    ? mainWindow.loadURL("about:blank")
-    : mainWindow.loadFile(path.join(__dirname, "index.html"));
-};
+    ? mainWindow.loadURL('about:blank')
+    : mainWindow.loadFile(path.join(__dirname, 'index.html'))
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on('ready', createWindow)
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-});
+})
 
-app.on("activate", () => {
+app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createWindow()
   }
-});
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
